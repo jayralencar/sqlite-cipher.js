21cd1cb0a0f53626bb81929db4eb9954938dc69a
JayrAlencar <jayralencarpereira@gmail.com>
2016-03-20 09:10:56 -0300

    Delete with prepared sql

--- test/app.js
+++ test/app.js
@@ -4,12 +4,12 @@ sqlite.connect('Teste.erwr','18021994');
 
 sqlite.run("CREATE TABLE COMPANYS(ID  INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT NOT NULL);");
 
-sqlite.insert("COMPANYS",{NAME:"My COMPANY Ãé"}, function(inserid){
+sqlite.insert("COMPANYS",{NAME:"COMPANY"}, function(inserid){
 	console.log(inserid);
 });
 
-console.log(sqlite.run("SELECT name FROM COMPANYS;"));
+sqlite.run("DELETE FROM COMPANYS WHERE NAME = '?'",['COMPANY']);
 
-sqlite.encrypt('test.db','test.aes','eirdjhas1293');
+console.log(sqlite.run("SELECT * FROM COMPANYS;"));
 
-sqlite.decrypt('test.aes','decrypted.db','eirdjhas1293');
+

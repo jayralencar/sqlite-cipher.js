9f632f541d183c8c4b8eb8953f18343dfb37b676
Jayr Alencar <jayralencarpereira@gmail.com>
2016-03-30 15:06:44 -0300

    Comments & tests

--- sqlite.js
+++ sqlite.js
@@ -1,27 +1,73 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Jayr Alencar
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+//Requeries
 var fs = require('fs');
 var crypto = require('crypto');
 
 function sqlite () {}
 
-sqlite.prototype = require('sqlite-sync');
+// Variables
+sqlite.prototype = require('sqlite-sync'); //inheriting from sqlite-sync.js
 sqlite.prototype.buffer = null;
 sqlite.prototype.file2 = null;
-sqlite.prototype.password = null;
+sqlite.prototype.password = 'clubeDosGeeks';
 sqlite.prototype.algorithm = 'aes-256-ctr';
 sqlite.prototype.algorithms = crypto.getCiphers();
 
-sqlite.prototype.pvDecrypt = function(buffer,password){
-  var decipher = crypto.createDecipher(this.algorithm,password)
+/**
+   * Buffer decryption
+   *
+   * @param {Object} buffer - Encrypted buffer
+   * @return {Object} - Decrypted buffer
+ */
+sqlite.prototype.pvDecrypt = function(buffer){
+  var decipher = crypto.createDecipher(this.algorithm,this.password)
   var dec = Buffer.concat([decipher.update(buffer) , decipher.final()]);
   return dec;
 }
 
-sqlite.prototype.pvEncrypt = function(buffer,password){
-  var cipher = crypto.createCipher(this.algorithm,password)
+/**
+   * Buffer encryption
+   *
+   * @param {Object} buffer - buffer
+   * @return {Object} - Encrypted buffer
+ */
+sqlite.prototype.pvEncrypt = function(buffer){
+  var cipher = crypto.createCipher(this.algorithm,this.password)
   var crypted = Buffer.concat([cipher.update(buffer),cipher.final()]);
   return crypted;
 }
 
+/**
+   * Database connection
+   *
+   * @param {String} db - File directory+filename
+   * @param {String} password 
+   * @param {String} algorithm
+   * @return {Object}
+ */
 sqlite.prototype.connect = function(db, password, algorithm){
 	this.file2 = db;
 	this.password = password || this.password;
@@ -34,7 +80,7 @@ sqlite.prototype.connect = function(db, password, algorithm){
 
 	if(fs.existsSync(db)){
 		var file2 = fs.readFileSync(db);
-		var file2buffer = this.pvDecrypt(file2, this.password);
+		var file2buffer = this.pvDecrypt(file2);
 		this.con(file2buffer);
 	}else{
 		this.con(1);
@@ -43,21 +89,35 @@ sqlite.prototype.connect = function(db, password, algorithm){
 	return this;	
 }
 
+/**
+   * Write the file
+   *
+   * @param {Object} buffer - buffer from sqlite-sync
+ */
 sqlite.prototype.writer = function(buffer){
-	var data = this.pvEncrypt(new Buffer(buffer, "utf-8"), this.password);
+	var data = this.pvEncrypt(new Buffer(buffer, "utf-8"));
 	var buffer = new Buffer(data);
-	console.log(this.file2)
 	fs.writeFileSync(this.file2, buffer);
 }
 
+/**
+   * File decryption
+   *
+   * @param {String} from - encrypted file way
+   * @param {String} to - decrypted file way
+   * @param {String} password 
+   * @param {String} algorithm
+   * @return {Object}
+ */
 sqlite.prototype.decrypt = function(from, to, password, algorithm){
 	if(fs.existsSync(from)){
 		this.algorithm = algorithm || this.algorithm;
+		this.password = password || this.password;
 		if(this.algorithms.indexOf(this.algorithm)==-1){
 			throw "This algorithm is not supported";
 		}
 		var file2 = fs.readFileSync(from);
-		var data = this.pvDecrypt(file2, password);
+		var data = this.pvDecrypt(file2);
 		var buffer = new Buffer(data);
 		fs.writeFileSync(to, buffer);
 	}else{
@@ -66,14 +126,24 @@ sqlite.prototype.decrypt = function(from, to, password, algorithm){
 	return this;	
 }
 
+/**
+   * File encryption
+   *
+   * @param {String} from - decrypted file way
+   * @param {String} to - encrypted file way
+   * @param {String} password 
+   * @param {String} algorithm
+   * @return {Object}
+ */
 sqlite.prototype.encrypt = function(from, to, password, algorithm){
 	if(fs.existsSync(from)){
 		this.algorithm = algorithm || this.algorithm;
+		this.password = password || this.password;
 		if(this.algorithms.indexOf(this.algorithm)==-1){
 			throw "This algorithm is not supported";
 		}
 		var db = fs.readFileSync(from);
-		var data = this.pvEncrypt(new Buffer(db,"utf-8"), password);
+		var data = this.pvEncrypt(new Buffer(db,"utf-8"));
 		var buffer = new Buffer(data);
 		fs.writeFileSync(to, buffer);
 	}else{
--- test/app.js
+++ test/app.js
@@ -1,16 +1,35 @@
-var sqlite = require('../sqlite.js');
+var sqlite = require('../sqlite.js'); //requiring
 
-sqlite.connect('test/Database.enc','myPass');
+//Connecting - (databaseFile, [password], [algorithm])
+sqlite.connect('test/Database.enc','myPass','aes-256-ctr');
 
+//Creating Table - you can run any command
 sqlite.run("CREATE TABLE COMPANYS(ID  INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT NOT NULL);");
 
+//Inserting - this function can be sync to, look the wiki
 sqlite.insert("COMPANYS",{NAME:"COMPANY"}, function(inserid){
 	console.log(inserid);
 });
 
-console.log(sqlite.run("SELECT * FROM COMPANYS;"));
+//Updating - returns the number of rows modified - can be async too
+var rows_modified = sqlite.update("COMPANYS",{NAME:"TESTING UPDATE"},{ID:1});
 
+//Create your function
+function concat(a,b){
+	return a+b;
+}
+
+//Add your function to connection
+sqlite.create_function(concat);
+
+// Use your function in the SQL
+console.log(sqlite.run("SELECT ID , concat(ID, NAME) as concat FROM COMPANYS;"));
+
+//Decrypting database file
 sqlite.decrypt("test/Database.enc","test/decrypted.db", 'myPass');
+
+//Encrypting database file
 sqlite.encrypt("test/decrypted.db","test/reencrypted.rec", 'myPass',"bf");
 
-
+// Closing connection 
+sqlite.close();
